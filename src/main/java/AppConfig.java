769fbc86ee5c299121d74754628bdcab34ef50e4
Lorenzo Sabbatini <lorenzo.sabbatini@exprivia.com>
2020-08-11 15:55:05 +0200

    Autowire con le StereoTypes Annotation

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,20 +4,25 @@ import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 // Annotazione a livello di classe che va a sostituire l'applicationContext.xml
 //
 // Avremo un registro di Spring con due Bean
+//
+// Commentato tutto il codice: i bean non sono più configurati ma vengono
+// creati facendo lo scan del package com.pluralsight e carcando le annotazione stereotype
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
     // Annotazione da usare su un metodo: ci dice che il metodo restituisce uno Spring Bean
     // e può essere usato all'interno della nostra applicazione
     //
     // NOTA: i bean creati sono tutti SINGLETON
-    @Bean(name="speakerService")
+/*    @Bean(name="speakerService")
     @Scope(value= BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService(){
         // Constructor Injection
@@ -29,14 +34,14 @@ public class AppConfig {
         // Setter Injection
         //service.setRepository(getSpeakerRepository());
         return service;
-    }
+    }*/
 
-    // NOTA: il nome dei bean è molto importante (auto-wiring)
     // In questo step non sembra importante il nome del bean: è importante che però Spring abbia registrato il bean
     // e che sia disponibile come candidato per una futura injection
-    // IL fatto che questo metodo restituisce un bean significa che HibernateSpeakerRepositoryImpl sarà creato una volta sola
-    @Bean(name="speakerRepository")
+    // IL fatto che questo metodo restituisce un bean significa che HibernateSpeakerRepositoryImpl sarà creato
+    // una volta sola
+    /*@Bean(name="speakerRepository")
     public SpeakerRepository getSpeakerRepository(){
         return new HibernateSpeakerRepositoryImpl();
-    }
+    }*/
 }
